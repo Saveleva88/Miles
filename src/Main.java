f9e07affab316c7961cfc4d00204bae7fcac039a
Irina Saveleva <fomina_88@mail.ru>
2021-10-12 14:56:48 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 25_460;
+        int milePrice = 20;
+        int bonus = ticketPrice / milePrice;
+        System.out.println(bonus + "мили");
+
+
+
+
+    }
+}
